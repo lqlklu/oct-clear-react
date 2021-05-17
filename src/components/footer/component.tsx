54c4492329b439e804d6dd39099710b7545e9187
shizuku606 <shizuku606@qq.com>
2021-05-17 17:19:12 +0800

    add icp

--- src/components/footer/component.tsx
+++ src/components/footer/component.tsx
@@ -7,5 +7,21 @@ import "./style.scss";
 
 export const Footer: FC = observer(() => {
   const { t } = useTranslation();
-  return <Layout.Footer className="footer">{t("copyright")}</Layout.Footer>;
+  return (
+    <Layout.Footer className="footer">
+      <span>{t("copyright")}</span>
+      <a
+        className="icp"
+        href="http://beian.miit.gov.cn/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="https://pic3.zhimg.com/80/v2-d0289dc0a46fc5b15b3363ffa78cf6c7.png"
+          alt="police"
+        />
+        <span>蜀ICP备2021011946号</span>
+      </a>
+    </Layout.Footer>
+  );
 });
